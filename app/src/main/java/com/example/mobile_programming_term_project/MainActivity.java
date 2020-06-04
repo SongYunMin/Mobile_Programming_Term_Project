035a61d67dc8dbbbad54bb46e3a09d98b7248d72
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-06-04 16:01:31 +0900

    기본 계산기 Layout

--- app/src/main/java/com/example/mobile_programming_term_project/MainActivity.java
+++ app/src/main/java/com/example/mobile_programming_term_project/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.mobile_programming_term_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
