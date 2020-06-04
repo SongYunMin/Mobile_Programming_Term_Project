2efcec80f84e93d04359b50d987dda89883f7c49
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-06-04 18:30:06 +0900

    비트연산 Layout 추가

--- app/src/main/java/com/example/mobile_programming_term_project/MainActivity.java
+++ app/src/main/java/com/example/mobile_programming_term_project/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 public class MainActivity extends Activity {
 
+    // 결과 출력 EditText
+    EditText resultText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
