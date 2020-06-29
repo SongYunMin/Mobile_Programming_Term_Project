bb1e2b8e790a6fb976e17fd7efc01a6bcf216ba9
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-06-29 21:03:47 +0900

    update

---
+++ app/src/main/java/com/example/mobile_programming_term_project/HistoryActivity.java
@@ -0,0 +1,12 @@
+package com.example.mobile_programming_term_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
--- app/src/main/java/com/example/mobile_programming_term_project/MainActivity.java
+++ app/src/main/java/com/example/mobile_programming_term_project/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mobile_programming_term_project;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
@@ -10,7 +12,10 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -21,26 +26,28 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
     public Button[] buttons = null;
     public Button btn_XOR, btn_AND, btn_OR, btn_NOT, btn_eq, btn_C;
+    public Button storage, history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // 큐를 이용한 연결리스트 생성
-        final Queue<String> historyQueue = new LinkedList<String>();
-
+        // 연결리스트를 이용한 큐 생성
+        final Queue<String> resultQueue;
+        // 히스토리 기능을 위한 연결리스트 생성
+        final LinkedList<String> historyList = new LinkedList<>();
         // Status Bar 제거
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        final StringBuilder fileResult = new StringBuilder("");     // 파일 입출력 될 변수
         buttons = new Button[16];
         int[] btn_id = {R.id.btn_0, R.id.btn_1, R.id.btn_2, R.id.btn_3, R.id.btn_4, R.id.btn_5,
                 R.id.btn_6, R.id.btn_7, R.id.btn_8, R.id.btn_9, R.id.btn_add, R.id.btn_sub,
                 R.id.btn_multi, R.id.btn_division, R.id.btn_eq, R.id.btn_mod};
 
-        editText = findViewById(R.id.edit);
+        editText = findViewById(R.id.edit);                 // 결과 출력 editText
+        history = findViewById(R.id.history);               // 히스토리 기능 버튼
+        storage = findViewById(R.id.memory_btn);            // 파일 수식 입력 버튼
         // 버튼들의 ID 받아옴
         for (int value : btn_id) findViewById(value).setOnClickListener(mClickListener);
 
@@ -51,9 +58,7 @@ public class MainActivity extends AppCompatActivity {
         btn_NOT = (Button) findViewById(R.id.btn_NOT);
         btn_eq = (Button) findViewById(R.id.btn_eq);
         btn_C = (Button) findViewById(R.id.btn_c);
-        String buf = "Test String Code";
 
-        // Final 선언을 해주어야 inner Class 접근가능
         // 중위표기 -> 후위표기식 객체 선언
         final getPostFix infixToPostFix = new getPostFix(STACK_MAX_SIZE);
         // 후위표기식 계산 기능 객체 선언
@@ -67,10 +72,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // result
+        // '=' 를 누를때 발생하는 이벤트
+        resultQueue = new LinkedList<String>();
         btn_eq.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                final StringBuilder fileResult = new StringBuilder("");     // 파일 입출력 될 변수
                 String infix = editText.getText().toString();       // EditText에서 얻음
                 fileResult.append(editText.getText().toString());   // 먼저 계산식을 붙임
                 StringBuilder buf = new StringBuilder();            // 버퍼 필요
@@ -91,15 +98,46 @@ public class MainActivity extends AppCompatActivity {
                             new FileWriter(getFilesDir() + "data.txt", true));
                     fileWriter.write(String.valueOf(fileResult));
                     // 연결리스트를 이용한 큐에 저장
-                    historyQueue.offer(String.valueOf(fileResult));
+                    resultQueue.offer(String.valueOf(fileResult));
                     // fileResult 초기화
                     fileResult.delete(0, fileResult.length());
                     // 파일 닫음
                     fileWriter.newLine();
                     fileWriter.close();
                 } catch (IOException e) {
+                    Log.e("ERROR : ","File I/O Error");
                     e.printStackTrace();
                 }
+            }
+        });
+
+        // TODO : 내장메모리 입력 기능 구현
+        storage.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("내장메모리 : ","테스트입니다.");
+                try {
+                    String buf;
+                    BufferedReader fileReader = new BufferedReader(
+                            new FileReader(getFilesDir() + "data.txt"));
+                    buf = fileReader.readLine();
+                    editText.setText(buf);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // TODO : 히스토리 기능 구현
+        history.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HistoryActivity.class);
+                startActivity(intent);
+                Log.i("HISTORY : ","테스트입니다.");
             }
         });
 
