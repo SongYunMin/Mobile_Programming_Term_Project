bcbf9622f5d399e0b2eb8fbeedbe2f19b2249bd3
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-07-01 19:00:40 +0900

    Quiz 기능 Test Code Update

--- app/src/main/java/com/example/mobile_programming_term_project/QuizActivity.java
+++ app/src/main/java/com/example/mobile_programming_term_project/QuizActivity.java
@@ -3,12 +3,36 @@ package com.example.mobile_programming_term_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class QuizActivity extends AppCompatActivity {
+    public String[] example;
+    public TextView workBook;
+    public EditText answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        workBook = findViewById(R.id.example);
+        answer = findViewById(R.id.result);
+        example = new String[]{" 10+5-3", " 13+23*13", " 51+87*2", " 10/5*3", " 45+65-3", " 98*5/13",
+                " 23+51-3", " 10-5*23", " 82+5*61", " 104-52*13", " 41*21+23"};
+        try {
+            BufferedWriter fileWriter = new BufferedWriter(
+                    new FileWriter(getFilesDir() + "quiz.txt", true));
+            for (int i = 0; i < example.length; i++) {
+                fileWriter.write(example[i]);
+                fileWriter.newLine();
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Test Code
 //        String test = " 10+5-3";
@@ -23,10 +47,15 @@ public class QuizActivity extends AppCompatActivity {
 //        System.out.println(testint);
 
     }
-
     @Override
-    protected  void onResume() {
+    protected void onResume() {
         super.onResume();
+        String buf;
+
+        workBook.setText(example[(int)((Math.random()*10000)%10)]);
+
+        System.out.println("test");
+
 
     }
 
